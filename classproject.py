c072a86679d2b2fe4eb19eb7b5d81fe60503e2df
root <you@example.com>
2019-01-24 02:26:07 +0000

    template added

--- classproject.py
+++ classproject.py
@@ -1,6 +1,17 @@
 from flask import Flask
+from flask import render_template
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def get_index():
+    return 'Index!'
+
+@app.route('/hello')
+def get_hello():
     return 'Hello, World!'
+
+@app.route('/hello<name>')
+def get_hello(name):
+    return 'Hello, ' +name+ '!'
+    return render_template('hello.html', name=name)
